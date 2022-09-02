c9d5e9ecd22c34418f2ecea32cf5f4ece9ac499c
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-09-02 19:40:18 +0200

    arrays and objects

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,18 @@
 let currentResult = 0;
 
+let names = [];
+
+names.push('luigi');
+names.push('peach');
+
+let person = {
+  name: 'mario',
+  age: 31,
+};
+
 function show() {
   currentResult = currentResult + userInput.value;
-  outputResult(currentResult, 'test');
+  outputResult(currentResult, names[1]);
 }
 
 addBtn.addEventListener('click', show);
