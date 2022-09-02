cd2254bfc4ef412e7df70dff7b839c0a2c10c97d
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-09-02 04:55:45 +0200

    shadow variables

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,10 +1,9 @@
-let num1 = 4;
-let num2 = 5;
+let myname = 'mario';
 
-let calculateDescription = `(${num1} + ${num2})`;
-
-outputResult(add(num1, num2), calculateDescription);
-
-function add(num1, num2) {
-  return (theResult = num1 + num2);
+function show(name) {
+  let myname = name;
+  alert(name);
 }
+
+myname = 'luigi';
+show('mario');
